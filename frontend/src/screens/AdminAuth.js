559c538bb82a80ddde948938c94dd3bc088ba66e
Amarjeet Kumar <amarjeetkr23034@gmail.com>
2023-02-07 03:59:20 +0530

    adminfrontend

--- frontend/src/screens/AdminAuth.js
+++ frontend/src/screens/AdminAuth.js
@@ -1,5 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import './AdminAuth.css';
 
 export default function AdminAuth() {
-  return <div></div>;
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const submitHandler = async (e) => {
+    try {
+      e.preventDefault();
+      const { data } = await axios.post('/api/v1/admin/auth', {
+        email,
+        password,
+      });
+      if (data.token) {
+        console.log(data.token);
+        localStorage.setItem('adminToken', data.token);
+      }
+    } catch (err) {
+      console.log(err.message || 'error in sign in');
+    }
+  };
+  return (
+    <div className="small-container">
+      <div id="main-container">
+        <div id="main-heading">
+          <p>Admin Auth</p>
+        </div>
+        <form onSubmit={submitHandler}>
+          <div>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter email"
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter password"
+            />
+          </div>
+          <div>
+            <input type="submit" value="Sign In" />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
--- frontend/src/screens/AdminDashboard.js
+++ frontend/src/screens/AdminDashboard.js
@@ -1,5 +1,76 @@
 import React from 'react';
+import './AdminDashboard.css';
 
 export default function AdminDashboard() {
-  return <div></div>;
+  return (
+    <div id="small-container">
+      <div id="create-product">
+        <p>Create Product</p>
+        <form>
+          <div>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="product name"
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="number"
+              name="price"
+              id="price"
+              placeholder="product price"
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="number"
+              name="rating"
+              id="rating"
+              placeholder="initial rating"
+              required
+              min={0}
+              max={5}
+            />
+          </div>
+          <div>
+            <input
+              type="number"
+              name="numReviews"
+              id="numReviews"
+              required
+              placeholder="initial number of review"
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="type"
+              id="type"
+              placeholder="shoe type"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="imgUrl-select">img url</label>
+            <select name="imgUrl" id="imgUrl-select" required>
+              <option value="/images/products/shoe1.jpg">shoe1.jpg</option>
+              <option value="/images/products/shoe3.png">shoe3.png</option>
+              <option value="/images/products/shoe2.jpg">shoe2.jpg</option>
+            </select>
+          </div>
+          <div>
+            <input type="submit" value="Create" />
+          </div>
+        </form>
+      </div>
+      <div id="delete-product">
+        <div></div>
+      </div>
+      <div id="message"></div>
+    </div>
+  );
 }
